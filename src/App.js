a9fd396292705ef1f37742259fb00832903611e0
meriambs <bensalah.meriem0@gmail.com>
2021-10-07 16:17:01 +0100

    check point state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+// import photo from '../src/1.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      Fullname: "Dellai Mohamed Ali",
+      Bio: "Mind your own bio",
+      Profession: "student",
+   
+      show: true,
+      counter:0
+    }
+
+  }
+  componentDidMount() {
+    console.log("state did mount");
+    setInterval(() => {
+
+      this.setState({
+        counter: this.state.counter + 1
+      })
+      console.log(this.state.counter)
+    }, 1000);
+
+  }
+
+  componentWillUnmount() {
+    alert(`The state lasted ${this.state.counter} seconds !`)
+  }
+
+
+  render() {
+    return (
+      <div className="info">
+        {(this.state.show) ? (<>
+          <h3> {this.state.Fullname} </h3>
+          <h3> {this.state.Bio} </h3>
+          <h3> {this.state.Profession} </h3>
+          <img src={this.state.imgsrc} />
+          <br />
+        <button onClick={() => this.setState({ show: !this.state.show })}> Hide info </button>
+        </>) : 
+        <button onClick={() => this.setState({ show: !this.state.show })} > show info </button>
+        }
+
+      </div>
+
+    )
+
+  }
 }
 
 export default App;
